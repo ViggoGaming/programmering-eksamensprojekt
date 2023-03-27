977bc5a3fe5b2ffbf1ef4ef8e5d027dbdec9326a
Victor Buch-Hansen <me@victorbuch.dk>
2023-03-27 13:42:24 +0200

    added frontend

--- frontend/src/FetchFoods.tsx
+++ frontend/src/FetchFoods.tsx
@@ -7,11 +7,12 @@ function Food() {
   const [foodResults, setFoodResults] = useState([]);
 
   async function fetchFoods() {
-    const url = "http://localhost:8000/api/foods";
+    const url = "https://programmering-eksamensprojekt-production.up.railway.app/api/food";
     const response = await fetch(url);
     const data = await response.json();
+    const foods = data["data"]
     console.log(data)
-    setFoodResults(data)
+    setFoodResults(foods)
   }
 
   useEffect(() => {
@@ -21,7 +22,7 @@ function Food() {
   return (
     <div className="cards">
       {foodResults.map(food => (
-        <Food_card name={food['name']} description={food['description']} image={food['image']} price={food['price']}/>
+        food['visible'] === true && <Food_card name={food['name']} description={food['description']} image={food['image']} price={food['price']} />
       ))}
     </div>
 
--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -8,6 +8,7 @@ import App from './App'
 
 function Header() {
     return (
+        <div className="sticky-top"> {/* Add a wrapper div with the sticky-top class */}
             <Navbar bg="light" expand="lg">
                 <Container>
                     <Navbar.Brand href="/">
@@ -24,11 +25,12 @@ function Header() {
                         <Nav className="me-auto">
                             <Nav.Link as={Link} to={"/"}>Hjem</Nav.Link>
                             <Nav.Link as={Link} to={"/"}>Ugensmenu</Nav.Link>
-                            <Nav.Link as={Link} to={"/add"}>tilføj</Nav.Link>
+                            <Nav.Link as={Link} to={"/add"}>Tilføj</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+        </div>
     );
 }
 
