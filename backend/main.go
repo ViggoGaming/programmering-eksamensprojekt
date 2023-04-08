0ef97888d86c165f32b17545b59b56042e2c2a03
Victor Buch-Hansen <me@victorbuch.dk>
2023-04-08 14:50:02 +0200

    updated cors

--- backend/main.go
+++ backend/main.go
@@ -15,7 +15,7 @@ func main() {
 
 	// Setup cors so the backend can share cookies with the frontend
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000, https://programmering-eksamensprojekt.vercel.app",
+		AllowOrigins:     "programmering-eksamensprojekt.vercel.app, localhost:3000",
 		AllowCredentials: true,
 	}))
 	router.SetupRoutes(app)
